3df1a724110145a327ef78338640e2c43f10b436
Vitor da Silva <vitordasilva.developer@gmail.com>
2021-04-16 11:53:31 -0300

    Commit inicial do projeto Loucademia

---
+++ src/main/java/br/com/softblue/loucademia/Config.java
@@ -0,0 +1,10 @@
+package br.com.softblue.loucademia;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class Config {
+
+}
