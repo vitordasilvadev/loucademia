1bb3cfb61f0cd3453c7374aaeffb48b1e0e40c88
Vitor da Silva <vitordasilva.developer@gmail.com>
2021-05-27 16:53:01 -0300

    Configurando os arquivos para as mensagens de validação via JSF

---
+++ src/main/java/br/com/softblue/loucademia/application/util/ValidationException.java
@@ -0,0 +1,25 @@
+package br.com.softblue.loucademia.application.util;
+
+public class ValidationException extends Exception {
+
+	public ValidationException() {
+		
+	}
+
+	public ValidationException(String message) {
+		super(message);
+	}
+
+	public ValidationException(Throwable cause) {
+		super(cause);
+	}
+
+	public ValidationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ValidationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
